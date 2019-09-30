a1c9699b593fc1befc2d961474143488233a5a4d
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-30 01:03:14 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,12 +7,15 @@ class Person
   end
 
   def get_married(person)
+    
     self.partner = person
+    
     if person.class != Person 
       raise PartnerError
     else 
       person.partner = self
     end 
+    
   end
   
   class PartnerError < StandardError 
